edb64747167ac58e520dd9fa387ed2a22c3a95a2
Sudip Sharma <sudpokhara826@gmail.com>
2024-10-07 22:58:47 +0545

    Edit and Deleted Features

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,11 @@ from .views.main_views import home,create_blog,single_blog,edit_blog,delete_blog
 from .views.auth_views import register,login
 
 urlpatterns = [
-    path("",home, name="home"), 
-    path("register/",register,name="register"), 
-    path("login/",login, name="login"), 
-    path("create/",create_blog), 
-    path("<int:blog_id>",single_blog), 
-    path("edit/",edit_blog), 
-    path("<int:blog_id>/delete",delete_blog,name="delete_blog")
+    path("", home, name="home"), 
+    path("register/", register, name="register"), 
+    path("login/", login, name="login"), 
+    path("create/", create_blog, name="create_blog"), 
+    path("<int:blog_id>/", single_blog, name="single_blog"), 
+    path("edit/<int:blog_id>/", edit_blog, name="edit_blog"),  # Added missing comma
+    path("<int:blog_id>/delete/", delete_blog, name="delete_blog")  # Added trailing slash for consistency
 ]
--- blog/views/main_views.py
+++ blog/views/main_views.py
@@ -11,8 +11,28 @@ def single_blog(request,blog_id):
     blog = get_object_or_404(Blogs,pk=blog_id);
     return render(request,"main/single_blog.html",{"blog":blog})
 
-def edit_blog(request):
-    return render(request,"main/edit_blog.html")
+@login_required
+def edit_blog(request, blog_id):
+    # Retrieve the blog object before processing the request
+    blog = get_object_or_404(Blogs, pk=blog_id)
+
+    if request.method == "POST":
+        title = request.POST.get("title")
+        subtitle = request.POST.get("subtitle")
+        description = request.POST.get("description")
+        image = request.FILES.get("image")
+
+        # Update the blog object with new data
+        blog.title = title
+        blog.subtitle = subtitle
+        blog.description = description
+        if image:
+            blog.image = image
+        
+        blog.save()  # Save the updated blog object
+        return redirect("home")
+    
+    return render(request, "main/edit_blog.html", {"blog": blog})
 @login_required
 # Login to get acess to the create blog page
 def create_blog(request):
@@ -26,9 +46,13 @@ def create_blog(request):
         return redirect("home")
   
     return render(request,"main/create_blog.html")
-
+@login_required
 def delete_blog(request,blog_id):
     blog = get_object_or_404(Blogs,pk=blog_id)
-    blog.delete()
-    return redirect("home")
+    if request.method == "POST":
+        # We need to deleted the blog if delete request is triggeded from the form post request
+      blog.delete()
+      return redirect("home")
+    else:
+       return redirect ("blog/"+str(blog_id))
         
